bf4321ab944b8d543318fe73e0973741eb97540b
marl0909 <taraskolesnyk0909@gmail.com>
2023-05-12 05:01:07 +0300

    init

--- src/components/Projects/ProjectBlock/RrojectBlock.jsx
+++ src/components/Projects/ProjectBlock/RrojectBlock.jsx
@@ -6,13 +6,13 @@ export const ProjectBlock = (props) => {
 
     return (
         <div className={'project-item'}>
-            <a  className={'project-item__photo'} href={props.projectUrl} target={'_blank'}>
+            <a  className={'project-item__photo'} href={props.projectUrl} target='_blank'>
                 <img src={props.projectPhoto}/>
                 <div>
                     <span>{props.projectName}</span>
                 </div>
             </a>
-            <a className={'project-item__git-photo'} href={props.gitUrl}>
+            <a className={'project-item__git-photo'} href={props.gitUrl} target='_blank' >
                 <img src={githubIcon}/>
             </a>
             <div className={'project-item__description'}>
